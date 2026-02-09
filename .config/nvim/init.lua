9c2a1ba66f71c847e8701cf3e1b6e23c80f54c12
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-02-09 14:17:59 +0700

    .zshrc: update
    added java's jdk path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,47 +1,11 @@
--- init.lua: nvim's main entry
-local keymaps = require('keymaps')
-local indentSpace = 2
+require("core.opts")
+require("core.keymaps")
 
--- prepend (put on first) lazyvim path to runtimepath
-local lazypath = vim.fn.stdpath("data") .. "/site/pack/plugins/opt/lazy.nvim" -- vim.fn.stdpath("data") will return "/home/$USER/.local/share/nvim"
-vim.opt.rtp:prepend(lazypath)
+require("core.lazy")
+require("default-theme")
 
--- import lazyvim
-require("lazy").setup("plugins") -- search for ~/.config/nvim/lua/ -> plugins.lua OR plugins/init.lua
-
--- manual plugins (moved to ~/.local/share/nvim/site/pack/plugins/start/)
---[[require('lualine').setup({
-  options = {
-    icons_enabled = true,
-    theme = 'ayu_dark',
-    section_separators = '',
-    component_separators = ''
-  }
-})
-
-require('nvim-cursorline').setup {
-  cursorline = {
-    enable = true,
-    timeout = 1000,
-    number = false
-  },
-  cursorworld = {
-    enable = true,
-    min_length = 3,
-    h1 = { underline = true },
-  }
-} ]]--
-
--- my keymaps 
-keymaps.general()
-keymaps.vscode()
-
--- general configs
-vim.cmd('syntax on')
-vim.opt.number = true -- show line numbers
-vim.opt.smartindent = true
-vim.opt.expandtab = true -- change tab from \t to space based
-vim.opt.tabstop = indentSpace -- change \t found in the file to n space
-vim.opt.softtabstop = indentSpace
-vim.opt.shiftwidth = indentSpace -- how many spaces for automate indent/unindent with tab
-
+-- helper funcs
+function R(name)
+  package.loaded[name] = nil
+  return require(name)
+end
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,8 @@
--- main entry for nvim plugins
-local plugins_manual = {}
-local plugins_lazy = {}
+-- messy plugins that dont deserve a dedicated file
 
--- these are manual (local) plugins, manual dir path is needed) 
-plugins_manual = {
-  {
+return {
+  -- these are manual (local) plugins, manual dir path is needed)
+  --[[ {
     "nvim-lualine/lualine.nvim",
     dir = "~/.local/share/nvim/site/pack/plugins/start/lualine.nvim",
     config = function()
@@ -14,10 +12,10 @@ plugins_manual = {
           theme = 'ayu_dark',
           section_separators = '',
           component_separators = ''
-       }
+        }
       })
     end
-  },
+  }, ]]
   {
     "ya2s/nvim-cursorline",
     dir = "~/.local/share/nvim/site/pack/plugins/start/nvim-cursorline",
@@ -35,11 +33,132 @@ plugins_manual = {
         }
       }
     end
-  }
-}
+  },
 
--- these are plugins managed by lazy
-plugins_lazy = {
-}
+  -- these are plugins managed by lazy
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", mode = "n", desc = "find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  mode = "n", desc = "live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",    mode = "n", desc = "find buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>",  mode = "n", desc = "find help" },
+      { "<leader>FF", "<cmd>Telescope<cr>",            mode = "n", desc = "open telescope" },
+    },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+        }
+      }
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup {
+        current_line_blame = true,
+      }
+    end
+  },
+  {
+    "qwavies/smart-backspace.nvim",
+    keys = {
+      { "<leader>bs", "<cmd>SmartBackspaceToggle<CR>", mode = "n" },
+    },
+    opts = {
+      enabled = true, -- enables/disables smart-backspace
+      silent = false, -- if set to false, it will send a notification if smart-backspace is toggled
+      disabled_filetypes = {
+        "md",
+        "txt",
+      }
+    }
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      exclude = {
+        filetypes = {
+          "dashboard",
+        },
+      }
+    }
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
 
-return vim.list_extend(plugins_manual, plugins_lazy)
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start,
+        { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
+  },
+  -- { 'saghen/filler-begone.nvim' },
+  {
+    'karb94/neoscroll.nvim',
+    opts = {
+      stop_eof = true,
+    },
+  },
+  {
+    "declancm/cinnamon.nvim",
+    version = "*", -- use latest release
+    opts = {
+      -- change default options here
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  {
+    'mrjones2014/legendary.nvim',
+    priority = 2006,
+    lazy = false,
+  },
+}
